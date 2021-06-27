6fae0d808c0811a5559ff13ac1056f3d59110141
Emeka Nwadiogbu <odumfilm@outlook.com>
2021-06-27 10:27:01 -0700

    Expense Chart

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
-  
-function App() {
+import React, { useState } from 'react';
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: "Messinger Funeral Services",
+    amount: 5000,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: "Viewing Hall", amount: 2500, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: "Transportation",
+    amount: 1000,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: "Canopy",
+    amount: 2000,
+    date: new Date(2021, 5, 12),
+  },
+];
+const App = () => {
+
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h2>Emmanuel Obiesie Nwadiogbu's Funeral Services</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 }
---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+
+  const [title, setTitle] = useState(props.title);
+  console.log('Expense Item evaluated by React');
+
+  const itemButton = () => {
+    setTitle('Updated!');
+    console.log(title);
+  };
+
+  return (
+    <li>
+
+      <Card className='expense-item'>
+        <ExpenseDate date={props.date} />
+        <div className='expense-item__description'>
+          <h2>{title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
+        </div>
+        <button onClick={itemButton}>Change Title</button>
+      </Card>
+    </li>
+  );
+}
+
+export default ExpenseItem;
